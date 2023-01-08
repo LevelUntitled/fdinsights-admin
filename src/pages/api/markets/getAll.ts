1a7b5685db0b04794ac6d8b085f063b04eb1070f
Ishan Jain <ishanjain1502@gmail.com>
2023-01-08 16:12:14 +0530

    ading markets and discipline

---
+++ src/pages/api/markets/getAll.ts
@@ -0,0 +1,9 @@
+import { type NextApiRequest, type NextApiResponse } from "next";
+
+import { prisma } from "../../../server/db/client";
+
+const examples = async (req: NextApiRequest, res: NextApiResponse) => {
+   ;
+};
+
+export default examples;
