f10432bc694efcb060371a55fe4d0377068bab84
vedant-11 <v.vedantsingh.11@gmail.com>
2023-01-09 20:34:08 +0530

    added client fields

--- src/pages/createClient.tsx
+++ src/pages/createClient.tsx
@@ -34,7 +34,6 @@ const CreateClient: FC = () => {
     }));
   };
 
-  //upload all the data in trpc
   const handleSubmit = (event) => {
     event.preventDefault();
 
@@ -69,7 +68,84 @@ const CreateClient: FC = () => {
         value={formData.industry}
         onChange={handleChange}
       />
-      {/* other form fields */}
+      <label htmlFor="industry">address Line 1</label>
+      <input
+        type="text"
+        name="industry"
+        value={formData.addLine1}
+        onChange={handleChange}
+      />
+      <label htmlFor="industry">address Line 2</label>
+      <input
+        type="text"
+        name="industry"
+        value={formData.addLine2}
+        onChange={handleChange}
+      />
+      <label htmlFor="industry">City</label>
+      <input
+        type="text"
+        name="industry"
+        value={formData.city}
+        onChange={handleChange}
+      />
+      <label htmlFor="industry">postcode</label>
+      <input
+        type="text"
+        name="industry"
+        value={formData.postcode}
+        onChange={handleChange}
+      />
+      <label htmlFor="industry">state</label>
+      <input
+        type="text"
+        name="industry"
+        value={formData.state}
+        onChange={handleChange}
+      />
+      <label htmlFor="industry">fiscalEnd</label>
+      <input
+        type="text"
+        name="industry"
+        value={formData.fiscalEnd}
+        onChange={handleChange}
+      />
+      <label htmlFor="industry">fiscal Start</label>
+      <input
+        type="text"
+        name="industry"
+        value={formData.fiscalStart}
+        onChange={handleChange}
+      />
+      <label htmlFor="industry">Sub start</label>
+      <input
+        type="text"
+        name="industry"
+        value={formData.subStart}
+        onChange={handleChange}
+      />
+      <label htmlFor="industry">Sub end</label>
+      <input
+        type="text"
+        name="industry"
+        value={formData.subEnd}
+        onChange={handleChange}
+      />
+      <label htmlFor="industry">notes</label>
+      <input
+        type="text"
+        name="industry"
+        value={formData.notes}
+        onChange={handleChange}
+      />
+      <label htmlFor="industry">Sub Domain</label>
+      <input
+        type="text"
+        name="industry"
+        value={formData.subDomain}
+        onChange={handleChange}
+      />
+
       <button type="submit">Create Client</button>
     </form>
   );
